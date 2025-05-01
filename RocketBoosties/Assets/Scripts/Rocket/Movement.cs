a5edf3e24d2a074037791cf63c5d660bf7805eca
THE-BEAST\jonat <jonathan.e.villegas@gmail.com>
2025-05-01 13:52:22 -0700

    Got the rocket rotation working. Re-factored the boosting to use the up vector. Refactored to separate functions updated on Update(). These may need to change in future if we don't want to check every frame but it seems fine. Taking into account delta time for consistent rotation. May need an options menu where the user can select a max rotation speed. Removed rigidbody from the platform. TODO: make platforms into prefabs.

--- RocketBoosties/Assets/Scripts/Rocket/Movement.cs
+++ RocketBoosties/Assets/Scripts/Rocket/Movement.cs
@@ -1,26 +1,62 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private InputAction thrust;
-    private Rigidbody theRigidbody;
+    // Which input we'd like to use for thrust. Set it in the script.
+    [FormerlySerializedAs("thrust")] [SerializeField] private InputAction inputThrust;
+    // Input for rotation. Set in script.
+    [SerializeField] private InputAction inputRot;
+    
+    [SerializeField] private float thrustForce = 1.0f;
+    
+    [SerializeField] private float rotationSpeed = 1.0f;
+    
+    // Used on Update().
+    private Rigidbody _theRigidbody;
+    private Vector3 _theRotation;
     private void OnEnable()
     {
-        thrust.Enable();   
+        inputThrust.Enable();  
+        inputRot.Enable();
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        theRigidbody = GetComponent<Rigidbody>();
+        _theRigidbody = GetComponent<Rigidbody>();
+        _theRotation = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (thrust.IsPressed() && theRigidbody)
+        CheckForThrust();
+        CheckForRotation();
+    }
+
+    void CheckForThrust()
+    {
+        if (inputThrust.IsPressed() && _theRigidbody)
         {
-            theRigidbody.AddRelativeForce(new Vector3(0, 0.01f, 0));
+            
+            _theRigidbody.AddRelativeForce(this.gameObject.transform.up * (thrustForce * Time.deltaTime));
         }
+    }
+
+    void CheckForRotation()
+    {
+        _theRotation.z = inputRot.ReadValue<float>() * Time.deltaTime * rotationSpeed;
+        //Debug.Log(theRotation.z + " is the rotation");
+        this.gameObject.transform.Rotate(_theRotation);
+    }
+    float GetThrustForce()
+    {
+        return thrustForce;
+    }
+
+    void SetThrustForce(float force)
+    {
+        thrustForce = force;
     }
 }
